cd1dcd079ce5a8fdd30c962084136a1661e22a15
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-12 15:08:19 +0300

    BlogContext içine DbSet<> ler oluşturuldu.

--- MyBlog.DataAccessLayer/Context/BlogContext.cs
+++ MyBlog.DataAccessLayer/Context/BlogContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,12 @@ namespace MyBlog.DataAccessLayer.Context
         {
             optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;initial catalog=DbMyBlog;integrated security=true");
         }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }

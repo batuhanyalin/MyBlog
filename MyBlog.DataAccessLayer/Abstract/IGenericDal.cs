e175b25b73d19108c93d87fa649ae683ab626241
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-12 15:51:11 +0300

    DAL>Abstract - Interface sınıfları oluşturuldu.

--- MyBlog.DataAccessLayer/Abstract/IGenericDal.cs
+++ MyBlog.DataAccessLayer/Abstract/IGenericDal.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace MyBlog.DataAccessLayer.Abstract
 {
-    public interface IGenericDal
+    public interface IGenericDal<T> where T : class //t entityden gelen bir isimlendirme | Dışarıdan gelen T değeri bir class olmak zorunda.
+        //Interfaceler genellikle I harfiyle adlandırılır.
     {
+        //BU METOTLAR BÜTÜN ENTİTYLER İÇİN GEÇERLİ OLACAKLAR.
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        List<T> GetListAll();
+        T GetById(int id);
     }
 }

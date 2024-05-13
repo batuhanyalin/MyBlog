d2437dd562e6601043cecdb494077c4a7292e412
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-13 11:45:55 +0300

    BusinessLayer>Abstract Serviceler oluşturuldu ve kodları yazıldı. Concrete klasörü CategoryManager classı oluşturulup ICategoryService miras alınarak metotlar çağrıldı.

---
+++ MyBlog.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T:class // T İLE BURAYA PARAMETRE GÖNDERİLİYOR VE BU PARAMETRE MUTLAKA CLASS OLMAK ZORUNDA, GENERICSERVICE ÇAĞIRILDIĞI YERDE MUTLAKA BİR PARAMETRE VERİLMEK ZORUNDA OLUNACAK. -  DALdan gelen metotlar çağrılmadan önce validasyonlara tabi tutulacaklar.
+    {
+        //BURADA METOTLAR DATAACCESS KATMANIYLA ÇAKIŞMAMASI İÇİN METOT İSİMLERİNİN BAŞLARINA T HARFİ EKLENDİ.
+        void TInsert(T entity);
+        void TDelete(int id);
+        void TUpdate(T entity);      
+        List<T> TGetListAll();
+        T TGetById(int id);
+    }
+}

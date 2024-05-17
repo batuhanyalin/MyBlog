b6dce5bc8d53849b71aa280ad803f63a38bbb1a3
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-17 11:14:18 +0300

    BusinessLayer>ValidationRules içine validasyon kontrolleri için  validasyon sınıfları oluşturuldu ve içlerine constructor metot oluşturulup RuleFor ifadesiyle validasyonlar yazıldı.

---
+++ MyBlog.BusinessLayer/ValidationRules/ArticleValidation/CreateArticleValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MyBlog.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.BusinessLayer.ValidationRules.ArticleValidation
+{
+    public class CreateArticleValidation : AbstractValidator<Article>
+    {
+        public CreateArticleValidation()
+        {
+            RuleFor(X=>X.Title).NotEmpty().WithMessage("Başlık boş geçilemez.").MinimumLength(3).WithMessage("Başlık değeri en az 3 karakter olmak zorundadır.").MaximumLength(3).WithMessage("Başlık değeri en fazla 20 karakter olmak zorundadır.");
+            RuleFor(x=>x.Detail).NotEmpty().WithMessage("Detaylar bölümü boş geçilemez.");
+        }
+    }
+}
---
+++ MyBlog.BusinessLayer/ValidationRules/CategoryValidation/CreateCategoryValidation.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using MyBlog.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.BusinessLayer.ValidationRules.CategoryValidation
+{
+    public class CreateCategoryValidation:AbstractValidator<Category> //Abstracvalidator çağırılıp içine ilgili sınıf ekleniyor
+    {
+        public CreateCategoryValidation()//Validasyon için gerekli metot ancak metotla birlikte çağırılabilir, bundan dolayı CreateCategoryValidation classı içine constructur method tanımlayıp RuleFor metodunu bu şekilde çağırabiliriz.
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adı boş geçilemez.");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter veri girişi yapınız.");
+            RuleFor(x => x.CategoryName).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter veri girişi yapınız.");
+        }
+    }
+}

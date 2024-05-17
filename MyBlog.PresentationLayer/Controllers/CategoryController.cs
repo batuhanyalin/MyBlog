b8456ac225497dfbe0fdeb06828b6f55fee6da6d
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-17 11:36:17 +0300

    PresentationLayer>CategoryController ve CategoryIndex IActionResultu için (listeleme) view oluşturuldu.

---
+++ MyBlog.PresentationLayer/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+
+namespace MyBlog.PresentationLayer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService; // Business katmanındaki metotlara erişmek için dependency injection uygulayarak field çağırılıyor. Interface çağırıldığında bağımlılık kalmıyor. Yoksa CategoryManager classı da çağrılabilirdi.
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index() 
+        {
+            var values=_categoryService.TGetListAll();
+            return View(values);
+        }
+    }
+}

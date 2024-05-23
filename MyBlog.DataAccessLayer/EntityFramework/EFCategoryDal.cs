fd1991fc4319d2bb22f006a1564c2bdd93156e21
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-23 12:19:40 +0300

    EntityLayer' a identityEntityframeworkCore paketi kuruldu.

--- MyBlog.DataAccessLayer/EntityFramework/EFCategoryDal.cs
+++ MyBlog.DataAccessLayer/EntityFramework/EFCategoryDal.cs
@@ -11,17 +11,17 @@ namespace MyBlog.DataAccessLayer.EntityFramework
 {
     public class EFCategoryDal : GenericRepository<Category>, ICategoryDal //efcategorydal hem genericrepository hem icategorydal interfaceinden miras alıyor. * Entityi repository ile haberleştiriyor.
     {
-        private readonly ICategoryDal _categoryDal;
+        //private readonly ICategoryDal _categoryDal;
 
-        public EFCategoryDal(ICategoryDal categoryDal)
-        {
-            _categoryDal = categoryDal;
-        }
+        //public EFCategoryDal(ICategoryDal categoryDal)
+        //{
+        //    _categoryDal = categoryDal;
+        //}
 
-        public int GetCategoryCount()
-        {
-            var value = _categoryDal.GetListAll().GroupBy(x => x.CategoryId).Count();
-            return value;
-        }
+        //public int GetCategoryCount()
+        //{
+        //    var value = _categoryDal.GetListAll().GroupBy(x => x.CategoryId).Count();
+        //    return value;
+        //}
     }
 }
---
+++ MyBlog.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultFooterSocialMediaComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+
+namespace MyBlog.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultFooterSocialMediaComponentPartial : ViewComponent
+    {
+        private readonly ISocialMediaService _socialMediaService;
+
+        public _DefaultFooterSocialMediaComponentPartial(ISocialMediaService socialMediaService)
+        {
+            _socialMediaService = socialMediaService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var value = _socialMediaService.TGetListAll();
+            return View(value);
+        }
+    }
+}

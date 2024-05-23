29233c00c8f3433e529037d76e6b3cd827e69b11
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-23 16:37:00 +0300

    Models içerisine CustomIdentityValidator sınıfı oluşturup IdentityErrorDescriber sınıfından miras aldırıldı ve override edilerek IdentityError metotları eklendi. Hata mesajları hedef alındı ve hataların Türkçeleştirilmeleri sağlandı.

---
+++ MyBlog.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyBlog.PresentationLayer.Models
+{
+    public class CustomIdentityValidator:IdentityErrorDescriber
+    {
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Lütfen en az 1 tane küçük harf girişi yapınız ('a'-'z')."
+            };
+        }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Lütfen en az 1 tane büyük harf girişi yapınız ('A'-'Z')."
+            };
+        }
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = "Lütfen en az 6 karakter veri girişi yapınız."
+            };
+        }
+    }
+}

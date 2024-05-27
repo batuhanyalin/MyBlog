ef958aaf1ca1b096c23b2e60711e9cae03d99800
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-27 15:59:26 +0300

    BlogController>MyBlogList ActionResultında, oluşturulan metot çağırılıp viewe gönderiliyor.

--- MyBlog.PresentationLayer/Areas/Writer/Controllers/BlogController.cs
+++ MyBlog.PresentationLayer/Areas/Writer/Controllers/BlogController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+using MyBlog.EntityLayer.Concrete;
 
 namespace MyBlog.PresentationLayer.Areas.Writer.Controllers
 {
     [Area("Writer")] //Area tanıtılıyor.
     public class BlogController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IArticleService _articleService;
+        public BlogController(UserManager<AppUser> userManager, IArticleService articleService)
+        {
+            _userManager = userManager;
+            _articleService = articleService;
+        }
         public IActionResult MyBlogList() //Giriş yapan yazara ait blogları tutacak olan
         {
-            return View();
+            var user = _userManager.FindByNameAsync(User.Identity.Name);
+            var values = _articleService.TGetArticlesByWriter(user.Id);
+            return View(values);
+
         }
     }
 }

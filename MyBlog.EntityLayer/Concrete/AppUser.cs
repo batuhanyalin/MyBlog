0b720416940e9f08b867d382a109f08b50cd14d3
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-27 15:29:16 +0300

    AspNetUser ile Article sınıfı arasında ilişki oluştruldu.

--- MyBlog.EntityLayer/Concrete/AppUser.cs
+++ MyBlog.EntityLayer/Concrete/AppUser.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace MyBlog.EntityLayer.Concrete
 {
-    public class AppUser:IdentityUser<int> //Bu şekilde kullanıldığında SQLe yansıyan User sınıfındaki id değeri int değere dönüşür.
+    public class AppUser : IdentityUser<int> //Bu şekilde kullanıldığında SQLe yansıyan User sınıfındaki id değeri int değere dönüşür.
     {
         public string Name { get; set; }
         public string Surname { get; set; }
         public string? City { get; set; }
         public string? ImageUrl { get; set; }
+        public List<Article> Articles { get; set; }
     }
 }

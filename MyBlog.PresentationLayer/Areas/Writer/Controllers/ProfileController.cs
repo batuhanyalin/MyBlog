c57147bea13b0fac4ac17fcdf83e5e4d5bdb3dfc
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-05-29 10:40:32 +0300

    Area ProfileController içerisinde fotoğraf ekleme yapısı kuruldu.

---
+++ MyBlog.PresentationLayer/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.EntityLayer.Concrete;
+using MyBlog.PresentationLayer.Areas.Writer.Models;
+
+namespace MyBlog.PresentationLayer.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    [Route("Writer/Profile")] //Burada yönlendirme yapıyoruz. Area adı/Controller adı
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ProfileController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Route("EditProfile")] //Burada area sayfalarında yönlendirme yapıyoruz. Area adı/Controller adı/View adı
+        public async Task<IActionResult> EditProfile()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Email = values.Email;
+            model.PhoneNumber = values.PhoneNumber;
+            model.ImageUrl = values.ImageUrl;
+            model.City = values.City;
+            model.Surname = values.Surname;
+            model.Username = values.UserName;
+            return View(model);
+        }
+        [HttpPost]
+        [Route("EditProfile")]
+        public async Task<IActionResult> EditProfile(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Image != null)
+            {
+                var resource = Directory.GetCurrentDirectory(); //başlangıçtaki proje yolunu al.
+                var extension = Path.GetExtension(p.Image.FileName); //Dosyadan gelen adın extension ını al.
+                var imageName = Guid.NewGuid() + extension; //Burada Guid türetilerek aynı isimde dosya eklenip üst üste yazılma durumu önleniyor.
+                var saveLocation = resource + "/wwwroot/images/" + imageName; //Dosya yolu belirleniyor.
+                var stream = new FileStream(saveLocation, FileMode.Create); //Dosya okuma ve yazma izinleri için kullanılan bir sınıftır (2. overloadına gidiliyor)
+                await p.Image.CopyToAsync(stream);
+                user.ImageUrl = imageName;
+            }
+            p.Name = user.Name;
+            p.Email = user.Email;
+            p.PhoneNumber = user.PhoneNumber;
+            p.Surname = user.Surname;
+            p.City = user.City;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password); //password şifrelenerek alınıyor.
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                RedirectToAction("MyBlogList", "Blog", new { Area = "Writer" });
+            }
+            return View();
+        }
+    }
+}

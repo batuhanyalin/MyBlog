acd0b9859a4824b6b215f86784b9d63e1458d9a5
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-06-03 17:02:29 +0300

    AppUser sınıfına yazar sosyal medya linkleri eklendi

--- MyBlog.EntityLayer/Concrete/AppUser.cs
+++ MyBlog.EntityLayer/Concrete/AppUser.cs
@@ -16,5 +16,9 @@ namespace MyBlog.EntityLayer.Concrete
         public List<Article> Articles { get; set; }
         //MVCde Virtual kullanılırken burada gerekmiyor. Burada LazyLoad ve EagerLoad kavramları öne çıkıyor. Bütün veriye değil sadece ihtiyaç duyulan verinin yüklenmesi amaçlanıyor. Include kavramı burada devreye girecek. 
         public List<Comment> Comments { get; set; }
+        public string? InstagramUrl { get; set; }
+        public string? FacebookUrl { get; set; }
+        public string? TwitterUrl { get; set; }
     }
 }
+

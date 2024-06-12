9987505c6fb105b3374737911952645297a728ab
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-06-12 15:54:44 +0300

    AreaAdmin BlogController metotları yazıldı.

--- MyBlog.PresentationLayer/Areas/Admin/Controllers/BlogController.cs
+++ MyBlog.PresentationLayer/Areas/Admin/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+using MyBlog.EntityLayer.Concrete;
 
 namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
 {
@@ -6,10 +8,46 @@ namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
     [Route("Admin/Blog")]
     public class BlogController : Controller
     {
+        private readonly IArticleService _articleService;
+
+        public BlogController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
         [Route("Index")]
         public IActionResult Index()
         {
+            var values = _articleService.TGetListAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateBlog()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateBlog(Article p)
+        {
+            _articleService.TInsert(p);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteBlog(int id)
+        {
+            _articleService.TDelete(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateBlog(int id)
+        {
+            var values = _articleService.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateBlog(Article p)
+        {
+            _articleService.TUpdate(p);
+            return RedirectToAction("Index");
         }
     }
 }

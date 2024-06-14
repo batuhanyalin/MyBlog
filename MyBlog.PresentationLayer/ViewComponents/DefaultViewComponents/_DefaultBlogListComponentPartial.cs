93a8a13862939e0fec0ab6ae21d566215a4f5e0d
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-06-14 17:53:24 +0300

    FeaturePost kısmı düzeltildi ve 4e kadar öne çıkarılmış postu gösterir hale getirildi.

--- MyBlog.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultBlogListComponentPartial.cs
+++ MyBlog.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultBlogListComponentPartial.cs
@@ -17,7 +17,7 @@ namespace MyBlog.PresentationLayer.ViewComponents.DefaultViewComponents
 
         public IViewComponentResult Invoke()
         {
-            var values = _articleService.TGetArticlesWithCategory();
+            var values = _articleService.TGetArticlesWithCategoryForIsApproved();
             foreach (var x in values)
             {
                 x.ReadingTime = _articleService.TGetReadingTime(x.ArticleId);

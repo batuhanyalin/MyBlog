34ecc06a133eff623f8d4d514c2b35af99a57bfc
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-06-25 13:26:32 +0300

    Login ekranı düzenlendi.

--- MyBlog.PresentationLayer/Areas/Admin/Controllers/StatisticController.cs
+++ MyBlog.PresentationLayer/Areas/Admin/Controllers/StatisticController.cs
@@ -2,8 +2,11 @@
 
 namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Route("Admin/{controller}")]
     public class StatisticController : Controller
     {
+        [Route("Index")]
         public IActionResult Index()
         {
             return View();

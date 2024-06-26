8e9ec9ef8fcf3bcd90ed4e0008cbd99b4f41f8ed
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-06-26 21:22:28 +0300

    AutoMapper DependencyInjection paketi kuruldu

---
+++ MyBlog.PresentationLayer/Areas/Admin/Models/CategoryBlogCount.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBlog.PresentationLayer.Areas.Admin.Models
+{
+    public class CategoryBlogCount
+    {
+        public string Category { get; set; }
+        public int Count { get; set; }
+    }
+}
--- MyBlog.PresentationLayer/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutTableChartComponentPartial.cs
+++ MyBlog.PresentationLayer/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutTableChartComponentPartial.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+using MyBlog.PresentationLayer.Areas.Admin.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MyBlog.PresentationLayer.Areas.Admin.ViewComponents.AdminLayoutViewComponents
 {
     public class _AdminLayoutTableChartComponentPartial : ViewComponent
     {
+        private readonly IArticleService _articleService;
+        public _AdminLayoutTableChartComponentPartial(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _articleService.TGetArticlesWithCategory()
+                                        .GroupBy(a => a.Category.CategoryName)
+                                        .Select(g => new CategoryBlogCount
+                                        {
+                                            Category = g.Key,
+                                            Count = g.Count()
+                                        })
+                                        .ToList();
+            return View(values);
         }
     }
 }

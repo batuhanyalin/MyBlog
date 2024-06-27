dd9d4e4d1d85eb17e20d1ec61162f4b3ac1e1ffc
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-06-27 19:51:25 +0300

    Dashboard Message viewcomponent dakika kısmı revize edildi ve mesaj listesi getirildi.

--- MyBlog.PresentationLayer/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutStatisticComponentPartial.cs
+++ MyBlog.PresentationLayer/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutStatisticComponentPartial.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
 
 namespace MyBlog.PresentationLayer.Areas.Admin.ViewComponents.AdminLayoutViewComponents
 {
     public class _AdminLayoutStatisticComponentPartial : ViewComponent
     {
+        private readonly IArticleService _articleService;
+        private readonly ICommentService _commentService;
+
+        public _AdminLayoutStatisticComponentPartial(IArticleService articleService, ICommentService commentService)
+        {
+            _articleService = articleService;
+            _commentService = commentService;
+        }
+
         public IViewComponentResult Invoke()
         {
+            var values=_articleService.TGetListAll();
             return View();
         }
     }
--- MyBlog.PresentationLayer/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutWidgetsComponentPartial.cs
+++ MyBlog.PresentationLayer/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutWidgetsComponentPartial.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+using MyBlog.EntityLayer.Concrete;
 
 namespace MyBlog.PresentationLayer.Areas.Admin.ViewComponents.AdminLayoutViewComponents
 {
     public class _AdminLayoutWidgetsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IMessageService _messageService;
+        private readonly UserManager<AppUser> _userManager;
+
+        public _AdminLayoutWidgetsComponentPartial(IMessageService messageService, UserManager<AppUser> userManager)
         {
-            return View();
+            _messageService = messageService;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var message = await _messageService.TGetInboxMessage(user.Id);
+            return View(message);
         }
     }
 }
